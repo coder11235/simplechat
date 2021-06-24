ef8c5b2b1ec44d47e842c566e403a5842b2115ca
uday kalyan s <udaykalyansreenivasa@gmail.com>
2021-06-24 12:11:58 +0530

    hopefully made design better

--- clientapp/src/app/app.module.ts
+++ clientapp/src/app/app.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NicknameComponent } from './components/nickname/nickname.component';
 import { ChatComponent } from './components/chat/chat.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-// import { FormsModule } from '@angular/forms'
 
 import { MatCardModule } from '@angular/material/card'
 import { MatFormFieldModule } from'@angular/material/form-field'
@@ -24,12 +21,10 @@ import { CookieService } from 'ngx-cookie-service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
     MatCardModule,
     MatFormFieldModule,
-    // FormsModule,
     MatInputModule,
     MatButtonModule,
     MatDialogModule
--- clientapp/src/environments/environment.prod.ts
+++ clientapp/src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
   production: true
 };
+
+export default {
+  key: '1aaee790a4c938ac7c6f',
+  cluster: 'ap2'
+}
